a9fdc56acacd6082a6a2ce1a9be64e83f72462b3
Sai Dinesh <72199223+Saidinesh-21@users.noreply.github.com>
2025-05-26 10:07:43 +0530

    g

--- App.tsx
+++ App.tsx
@@ -112,7 +112,7 @@ const App: React.FC = () => {
           name="Vamshi Mailaram"
           professionalSummary="Passionate visual storyteller, capturing moments and crafting narratives through photography and videography."
           profileImageUrl="https://picsum.photos/seed/vamshi_profile/200/200"
-          backgroundImageUrl="Images/Background.jpg" 
+          backgroundImageUrl="/Images/Background.jpg" 
         />
         <Timeline events={sampleTimelineData} />
       </main>
