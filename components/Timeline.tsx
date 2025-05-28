58be4295a0871b76e9f759199c1b416ee40733b1
Sai Dinesh <72199223+Saidinesh-21@users.noreply.github.com>
2025-05-28 10:16:12 +0530

    a

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -22,7 +22,7 @@ const Timeline: React.FC<TimelineProps> = ({ events }) => {
 
   return (
     <div
-      className="mx-auto px-6 sm:px-10 lg:px-16 xl:px-24 py-8 sm:py-12 max-w-screen-xl"
+      className="mx-auto px-6 sm:px-10 lg:px-16 xl:px-24 pt-16 sm:pt-20 pb-8 sm:pb-12 max-w-screen-xl"
       style={{ boxSizing: 'border-box' }}
     >
       <div className="relative">
